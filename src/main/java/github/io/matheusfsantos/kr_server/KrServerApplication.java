854552cb8989a601d2a2f5699db2d5606c200ab2
Matheus-FSantos <matheus.fs.contato@gmail.com>
2026-07-03 23:44:23 -0300

    feat: add base server-side (back-end) project

---
+++ src/main/java/github/io/matheusfsantos/kr_server/KrServerApplication.java
@@ -0,0 +1,13 @@
+package github.io.matheusfsantos.kr_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/io/matheusfsantos/kr_server/KrServerApplicationTests.java
@@ -0,0 +1,13 @@
+package github.io.matheusfsantos.kr_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
